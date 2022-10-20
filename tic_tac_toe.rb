85b1f1f943b1cbcde5c83696dfa11e5df626e3ec
Justin Townley <justin.d.townley@gmail.com>
2022-10-20 09:45:08 -0700

    Added functionality to print ascii board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,1 +1,24 @@
+# frozen_string_literal: true
+
 require 'pry-byebug'
+
+spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def print_board(spaces)
+  puts "     |     |\n"\
+       "  #{spaces[6]}  |  #{spaces[7]}  |  #{spaces[8]}\n"\
+       "_____|_____|_____\n"\
+       "     |     |\n"\
+       "  #{spaces[3]}  |  #{spaces[4]}  |  #{spaces[5]}\n"\
+       "_____|_____|_____\n"\
+       "     |     |\n"\
+       "  #{spaces[0]}  |  #{spaces[1]}  |  #{spaces[2]}\n"\
+       "     |     |"
+end
+
+print_board(spaces)
+spaces[0] = 'X'
+spaces[4] = 'X'
+spaces[8] = 'X'
+print_board(spaces)
+print_board(spaces)
